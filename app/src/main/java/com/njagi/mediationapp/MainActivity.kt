d989094d40c2d1e059c8d6e5b287ef4573b35205
Harmaton <njagiiharmaton@gmail.com>
2022-10-12 13:07:58 +0300

    TopBar Complete

--- app/src/main/java/com/njagi/mediationapp/MainActivity.kt
+++ app/src/main/java/com/njagi/mediationapp/MainActivity.kt
@@ -3,15 +3,26 @@ package com.njagi.mediationapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.njagi.mediationapp.ui.theme.MediationAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,31 +31,73 @@ class MainActivity : ComponentActivity() {
         setContent {
             MediationAppTheme {
                 Column(
-                    modifier = Modifier.background(Color.Gray).
-                            fillMaxSize()
+                    modifier = Modifier
+                        .background(Color.Gray)
+                        .fillMaxSize()
 
-                ){
-
+                ) {
+                    HeaderProfileComponent()
                 }
             }
         }
     }
-    @Composable
-    fun HeaderProfileComponent(){
+
+
+}
+@Composable
+fun HeaderProfileComponent() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Gray)
+            .padding(start = 15.dp, end = 15.dp, top = 15.dp)
+    ) {
+
         Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxWidth().padding()
-        ){
+            verticalAlignment = Alignment.CenterVertically
+        )
+        {
+
+            Image(painter = painterResource(id = R.drawable.ic_baseline_self_improvement_24),
+                contentDescription ="profile picture"  ,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+            )
+
+            Column(
+                modifier = Modifier.padding(start = 10.dp)
+            ) {
+                Text(text = "Welcome Back,",
+                    fontFamily = FontFamily.Cursive,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 14.sp,
+                    textAlign = TextAlign.Start
+                )
+                Text(text = "Njagi SirSusten",
+                    fontFamily = FontFamily.SansSerif,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Start
+                )
+            }
 
         }
-    }
 
+        BadgedBox(badge = { Badge(backgroundColor = Color.Red)}) {
+            Icon(imageVector = Icons.Default.Notifications,
+                contentDescription ="Notifications badge" )
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun HeaderPreview(){
     MediationAppTheme {
-
+        HeaderProfileComponent()
     }
 }
