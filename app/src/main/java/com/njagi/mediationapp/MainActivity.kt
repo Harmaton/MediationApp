269e8444e2507ca4ad46bc3820bef9e26ef446c7
Harmaton <njagiiharmaton@gmail.com>
2022-10-12 13:33:24 +0300

    search box

--- app/src/main/java/com/njagi/mediationapp/MainActivity.kt
+++ app/src/main/java/com/njagi/mediationapp/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,6 +26,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.njagi.mediationapp.ui.theme.MediationAppTheme
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,13 +47,14 @@ class MainActivity : ComponentActivity() {
 
 
 }
+
 @Composable
 fun HeaderProfileComponent() {
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color.Gray)
+//            .background(Color.Gray)
             .padding(start = 15.dp, end = 15.dp, top = 15.dp)
     ) {
 
@@ -59,8 +63,9 @@ fun HeaderProfileComponent() {
         )
         {
 
-            Image(painter = painterResource(id = R.drawable.ic_baseline_self_improvement_24),
-                contentDescription ="profile picture"  ,
+            Image(
+                painter = painterResource(id = R.drawable.ic_baseline_self_improvement_24),
+                contentDescription = "profile picture",
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .size(50.dp)
@@ -70,13 +75,15 @@ fun HeaderProfileComponent() {
             Column(
                 modifier = Modifier.padding(start = 10.dp)
             ) {
-                Text(text = "Welcome Back,",
+                Text(
+                    text = "Welcome Back,",
                     fontFamily = FontFamily.Cursive,
                     fontWeight = FontWeight.Light,
                     fontSize = 14.sp,
                     textAlign = TextAlign.Start
                 )
-                Text(text = "Njagi SirSusten",
+                Text(
+                    text = "Njagi SirSusten",
                     fontFamily = FontFamily.SansSerif,
                     fontWeight = FontWeight.Medium,
                     fontSize = 20.sp,
@@ -86,18 +93,50 @@ fun HeaderProfileComponent() {
 
         }
 
-        BadgedBox(badge = { Badge(backgroundColor = Color.Red)}) {
-            Icon(imageVector = Icons.Default.Notifications,
-                contentDescription ="Notifications badge" )
+        BadgedBox(badge = { Badge(backgroundColor = Color.Red) }) {
+            Icon(
+                imageVector = Icons.Default.Notifications,
+                contentDescription = "Notifications badge"
+            )
         }
 
     }
 }
 
+@Composable
+fun SearchInputBox() {
+    OutlinedTextField(value = "",
+        onValueChange = {},
+        placeholder = { Text(text = "Search ...", fontFamily = FontFamily.Serif) },
+        leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = " ") },
+        trailingIcon = {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_baseline_filter_list_24),
+                contentDescription = ""
+            )
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 15.dp, end = 15.dp, top = 15.dp)
+            .background(Color.White, shape = RoundedCornerShape(8.dp)),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.LightGray,
+            unfocusedBorderColor = Color.White,
+            cursorColor = Color.LightGray,
+            trailingIconColor = Color.Black
+        )
+
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
-fun HeaderPreview(){
+fun HeaderPreview() {
     MediationAppTheme {
-        HeaderProfileComponent()
+        Column( modifier = Modifier.background(Color.LightGray)) {
+            HeaderProfileComponent()
+            SearchInputBox()
+        }
+
     }
 }
